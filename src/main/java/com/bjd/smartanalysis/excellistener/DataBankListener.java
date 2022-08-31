7d206a04095cc8b4bc7351e52ab939156e14532e
Binoculus <929477145@qq.com>
2022-08-31 15:47:54 +0800

    余额根据借贷进出进行判断

--- src/main/java/com/bjd/smartanalysis/excellistener/DataBankListener.java
+++ src/main/java/com/bjd/smartanalysis/excellistener/DataBankListener.java
@@ -14,7 +14,8 @@ public class DataBankListener extends AnalysisEventListener<DataBank> {
     private Integer projectId;
     private List<DataBank> bankList = new ArrayList<>();
 
-    public DataBankListener() {}
+    public DataBankListener() {
+    }
 
     public DataBankListener(DataBankService bankService, Integer projectId) {
         this.bankService = bankService;
@@ -24,31 +25,46 @@ public class DataBankListener extends AnalysisEventListener<DataBank> {
     @Override
     public void invoke(DataBank dataBank, AnalysisContext analysisContext) {
         dataBank.setProjectId(projectId);
-        DataBank lastBank = null;
-        // 获取bankList中最后一条BFKH为BFKH的数据
-        for (int i = 0; i < bankList.size(); i++) {
-            if(bankList.get(i).getBFZH().equals(dataBank.getBFZH())) {
-                lastBank = bankList.get(i);
+        if (dataBank.getJDBZ().equals("进")) {
+            if (Float.parseFloat(dataBank.getJYJE()) >= 0) {
+                dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE())));
+            } else {
+                dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE()) * -1));
             }
-        }
-        // 如果lastBank的JYYE大于dataBank的JYYE，则dataBank的JYJE转为浮点型设置为负数，否则设置为正数转为浮点型，并将数据最终转为字符串
-        if(lastBank != null) {
-            Float lastBankJYYE = Float.parseFloat(lastBank.getJYYE());
-            Float dataBankJYYE = Float.parseFloat(dataBank.getJYYE());
-            if(lastBankJYYE>dataBankJYYE){
-                if(Float.parseFloat(dataBank.getJYJE())>0){
-                    dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE()) * -1));
-                }else{
-                    dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE())));
-                }
-            }else{
-                if(Float.parseFloat(dataBank.getJYJE())<0){
-                    dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE()) * -1));
-                }else{
-                    dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE())));
+        } else if (dataBank.getJDBZ().equals("出")) {
+            if (Float.parseFloat(dataBank.getJYJE()) > 0) {
+                dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE()) * -1));
+            } else {
+                dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE())));
+            }
+        } else {
+            DataBank lastBank = null;
+            // 获取bankList中最后一条BFKH为BFKH的数据
+            for (int i = 0; i < bankList.size(); i++) {
+                if (bankList.get(i).getBFZH().equals(dataBank.getBFZH())) {
+                    lastBank = bankList.get(i);
                 }
             }
+            // 如果lastBank的JYYE大于dataBank的JYYE，则dataBank的JYJE转为浮点型设置为负数，否则设置为正数转为浮点型，并将数据最终转为字符串
+            if (lastBank != null) {
+                Float lastBankJYYE = Float.parseFloat(lastBank.getJYYE());
+                Float dataBankJYYE = Float.parseFloat(dataBank.getJYYE());
+                if (lastBankJYYE > dataBankJYYE) {
+                    if (Float.parseFloat(dataBank.getJYJE()) > 0) {
+                        dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE()) * -1));
+                    } else {
+                        dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE())));
+                    }
+                } else {
+                    if (Float.parseFloat(dataBank.getJYJE()) < 0) {
+                        dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE()) * -1));
+                    } else {
+                        dataBank.setJYJE(String.valueOf(Float.parseFloat(dataBank.getJYJE())));
+                    }
+                }
+            }
         }
+
         bankList.add(dataBank);
     }
 
